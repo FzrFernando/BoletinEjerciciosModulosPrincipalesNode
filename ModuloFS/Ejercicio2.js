5c906b0cb12e41109f6c16ed5ac71ef7f20a3594
Fernando Zamora <fernandozamorarico@gmail.com>
2023-10-03 14:26:06 +0200

    Ejercicio 3 a terminar

--- ModuloFS/Ejercicio2.js
+++ ModuloFS/Ejercicio2.js
@@ -1,6 +1,6 @@
 //Lee el contenido del archivo nuevoArchivo.txt que creaste en el ejercicio anterior utilizando el módulo fs.
 //Muestra el contenido del archivo por pantalla.Muestra el contenido del archivo por pantalla.
-let fs = require("fs");
+const fs = require("fs");
 
 fs.readFile("nuevoArchivo.txt",function(err, data) {
     console.log(data.toString());
--- ModuloFS/Ejercicio3.js
+++ ModuloFS/Ejercicio3.js
@@ -1,4 +1,19 @@
 // Abre el archivo nuevoArchivo.txt en modo de escritura y añade el texto "¡Esto es genial!" al contenido existente.
 // Verifica que el archivo se ha actualizado correctamente.
-let fs = require("fs");
+const fs = require("fs");
 
+//Archivo que queremos editar
+const nuevoArchivo = "nuevoArchivo.txt"
+
+//Leemos el archivo
+fs.readFile(nuevoArchivo,(err,data) => {
+    if (err) {
+        console.error("No existe el archivo:", err);
+        return;
+    }
+
+    //Ponemos el texto que vamos a añadir
+    const contenidoNuevo = data + "\n¡Esto es genial!";
+
+    
+})
