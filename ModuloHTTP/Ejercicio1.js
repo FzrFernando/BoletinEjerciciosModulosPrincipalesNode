090800bae1bccd2456009217b5ce20d60c9d47c2
FzrFernando <74764616+FzrFernando@users.noreply.github.com>
2023-10-05 19:51:21 +0200

    Empezando con Modulo HTTP

---
+++ ModuloHTTP/Ejercicio1.js
@@ -0,0 +1,3 @@
+// Crea un servidor HTTP utilizando el módulo http.
+// El servidor debe responder a todas las solicitudes con un mensaje "¡Hola desde mi servidor!" y un estado HTTP 200.
+// Escucha en el puerto 3000 y muestra un mensaje en la consola indicando que el servidor está en funcionamiento.
---
+++ ModuloProcess/Ejercicio3.js
@@ -0,0 +1,3 @@
+// Crea un script que escuche el evento de salida del proceso ('exit') usando process.on().
+// Cuando se dispare el evento, muestra un mensaje en la consola indicando que el proceso está saliendo.
+process.on()
